1e3653bfd047b0212185873419c9bec79f28fded
triley22 <triley09@gmail.com>
2017-04-04 15:07:18 -0700

    first commit

---
+++ lava.js
@@ -0,0 +1,51 @@
+//JavaScript Lava Game
+
+//two dimensional grid
+var simpleLevelPlan = [
+  "                      ",
+  "                      ",
+  "  x              = x  ",
+  "  x         o o    x  ",
+  "  x @      xxxxx   x  ",
+  "  xxxxx            x  ",
+  "      x!!!!!!!!!!!!x  ",
+  "      xxxxxxxxxxxxxx  ",
+  "                      "
+];
+
+//constructor builds a level object
+//level stores width and height along with two arrays (one for grid one for actors)
+//grid is represented as array of array
+//actors array holds objects that track current position and state of dynamic elements in level
+
+function Level(plan) {
+  this.width = plan[0].length;
+  this.height = plan.length;
+  this.grid = [];
+  this.actors = [];
+
+  for (var y = 0; y < this.height; y++) {
+    var line = plan[y], gridLine = [];
+    for (var x = 0; x < this.width; x++) {
+      var ch = line[x], fieldType = null;
+      var Actor = actorChars[ch];
+      if (Actor)
+        this.actors.push(new Actor(new Vector(x, y), ch));
+      else if (ch == "x")
+        fieldType = "wall";
+      else if (ch == "!")
+        fieldType = "lava";
+      gridLine.push(fieldType);
+    }
+    this.grid.push(gridLine);
+  }
+
+  this.player = this.actors.filter(function(actor) {
+    return actor.type == "player";
+  })[0];
+  this.status = this.finishDelay = null;
+}
+
+Level.prototype.isFinished = function() {
+  return this.status != null && this.finishDelay < 0;
+};
